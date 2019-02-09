90315f9e8c085090f89657fcd1558a899c303777
Ryn Daniels <ryn@travis-ci.org>
2019-02-09 13:21:16 +0100

    maybe fix some of the image stuff

--- raparperyn.sh
+++ raparperyn.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 # First, find the most recent .img file
 IMG_PATH="$(ls -lr *.img | head -n 1 | awk '{ print $9 }')"
@@ -9,18 +10,23 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   OFFSET="$(fdisk $IMG_PATH | grep files | awk '{ print $11 }')"
 fi
 
+echo "Going to mount $IMG_PATH with offset $OFFSET"
+
 mkdir -p /tmp/rawpi
-mount -o loop,offset=$OFFSET raw_pi_dough.img /tmp/rawpi # it's RO now
+mount -o loop,offset=$OFFSET $IMG_PATH /tmp/rawpi # it's RO now
 mkdir -p /tmp/newpi
 tar cf - /tmp/rawpi | (cd /tmp/newpi; sudo tar xfp -)
 # the filesystem in the iso is now RW at /tmp/newpi/tmp/rawpi
 
 echo "DOING THE FILE STUFF"
+mkdir ./etc
 cd /tmp/newpi/tmp/rawpi
 cat "cats are amazing" > ./etc/motd
 # TODO
 # probs gonna have a really bad for loop here
 
 echo "Done with that, baking the iso now"
-mkisofs -o $HOME/bakedpi.iso -b isolinux/isolinux.bin -c isolinux/boot.cat
--no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "Homemade Rhubarb Pie" /tmp/newpi/tmp/rawpi
+mkisofs -o $HOME/bakedpi.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V "Homemade Rhubarb Pie" /tmp/newpi/tmp/rawpi .
+
+echo "did a thing"
+file $HOME/bakedpi.iso
